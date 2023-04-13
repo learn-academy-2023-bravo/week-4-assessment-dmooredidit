6763410187cd86eb49f437dcfb618c57fa6dcc53
D'Andre Moore <126428981+dmooredidit@users.noreply.github.com>
2023-04-13 10:57:05 -0700

    Did as much as I could. Hard time with writing functions

--- code-challenges.test.js
+++ code-challenges.test.js
@@ -12,40 +12,123 @@
 
 // --------------------1) Create a function that takes in an array, removes the first item from the array and shuffles the remaining content.
 
+// psuedocode: Create a function named shuffleRemove that takes in an array, removes the first item from the array and shuffles the remaining content.
+// Input: Array
+// Output: Array with first item removed and the rest shuffled
+// create function called shuffleRemove that takes in and object and outputs an array
+// I'm thinking I can iterate over the array using the .random method
+// since purple needs to be deleted, i would also have to remove that string before i randomize it because i can just use the index number to get to it in the beginning
+
 // a) Create a test with an expect statement using the variable provided.
 // HINT: Check out this resource: https://jestjs.io/docs/expect#expectarraycontainingarray
 
-const colors1 = ["purple", "blue", "green", "yellow", "pink"]
-// Expected output example (can be a different order): ["yellow", "blue", "pink", "green"]
-const colors2 = [
-  "chartreuse",
-  "indigo",
-  "periwinkle",
-  "ochre",
-  "aquamarine",
-  "saffron"
-]
-// Expected output example (can be a different order): ["saffron", "aquamarine", "periwinkle", "indigo", "ochre"]
+// describe("shuffleRemove", () => {
+//   it("takes in an array, removes the first item from the array and shuffles the remaining content.", () => {
+//     const colors1 = ["purple", "blue", "green", "yellow", "pink"];
+//     // Expected output example (can be a different order): ["yellow", "blue", "pink", "green"]
+
+//     const colors2 = [
+//       "chartreuse",
+//       "indigo",
+//       "periwinkle",
+//       "ochre",
+//       "aquamarine",
+//       "saffron",
+//     ];
+//     // Expected output example (can be a different order): ["saffron", "aquamarine", "periwinkle", "indigo", "ochre"]
+//     expect(shuffleRemove(colors1)).toEqual(["yellow", "blue", "pink", "green"]);
+//     expect(shuffleRemove(colors2)).toEqual([
+//       "chartreuse",
+//       "indigo",
+//       "periwinkle",
+//       "ochre",
+//       "aquamarine",
+//       "saffron",
+//     ]);
+//   });
+// });
+
+// --->  FAIL  ./code-challenges.test.js
+//   shuffleRemove
+//   ✕ takes in an array, removes the first item from the array and shuffles the remaining content.
+
+// ● shuffleRemove › takes in an array, removes the first item from the array and shuffles the remaining content.
+
+//   ReferenceError: shuffleRemove is not defined
 
 // b) Create the function that makes the test pass.
+// psuedocode: Create a function named shuffleRemove that takes in an array, removes the first item from the array and shuffles the remaining content.
+// Input: Array
+// Output: Array with first item removed and the rest shuffled
+// create function called shuffleRemove that takes in and object and outputs an array
+// since purple needs to be deleted, i would also have to remove it by using .shift first
+// I'm thinking I can iterate over the array using the .random method
 
 // --------------------2) Create a function that takes in an object that contains up votes and down votes and returns the end tally.
 
+// psuedocode: Create a function named endTally that takes in an object that contains up votes and down votes and returns the end tally.
+// Input: Object
+// Output: number
+// create function called endTally that takes in and object and outputs a number(integer)
+
 // a) Create a test with expect statements for each of the variables provided.
 
-const votes1 = { upVotes: 13, downVotes: 2 }
-// Expected output: 11
-const votes2 = { upVotes: 2, downVotes: 33 }
-// Expected output: -31
+// describe("endTally", () => {
+//   it("takes in an object that contains up votes and down votes and returns the end tally.", () => {
+//     const votes1 = { upVotes: 13, downVotes: 2 };
+//     // Expected output: 11
+//     const votes2 = { upVotes: 2, downVotes: 33 };
+//     // Expected output: -31
+
+//     expect(endTally(votes1)).toEqual(11);
+//     expect(endTally(votes2)).toEqual(-31);
+//   });
+// });
+// --->  FAIL  ./code-challenges.test.js
+//   endTally
+//   ✕ takes in an object that contains up votes and down votes and returns the end tally. (1 ms)
+
+// ● endTally › takes in an object that contains up votes and down votes and returns the end tally.
+
+//   ReferenceError: endTally is not defined
 
 // b) Create the function that makes the test pass.
 
 // --------------------3) Create a function that takes in two arrays as arguments and returns one array with no duplicate values. STRETCH: Use the spread operator to pass in a dynamic number of arguments.
 
-// a) Create a test with an expect statement using the variables provided.
+// psuedocode: Create a function named noDups that takes in two arrays as arguments and returns one array with no duplicate values.
+// Input: 2 Arrays
+// Output: 1 Array
+// create function called noDups that takes in two arrays as arguments and returns one array with no duplicate values.
+// I think I can join the two Arrays with .join
+// must fine a methed to remove duplicates. Maybe .filter.
 
-const dataArray1 = ["array", "object", "number", "string", "Boolean"]
-const dataArray2 = ["string", "null", "Boolean", "string", "undefined"]
-// Expected output: ["array", "object", "number", "string", "Boolean", "null", "undefined"]
+// a) Create a test with expect statements for each of the variables provided.
+
+describe("noDups", () => {
+  it("takes in two arrays as arguments and returns one array with no duplicate values.", () => {
+    const dataArray1 = ["array", "object", "number", "string", "Boolean"];
+    const dataArray2 = ["string", "null", "Boolean", "string", "undefined"];
+    // Expected output: ["array", "object", "number", "string", "Boolean", "null", "undefined"]
+
+    expect(noDups(dataArray1, dataArray2)).toEqual([
+      "array",
+      "object",
+      "number",
+      "string",
+      "Boolean",
+      "null",
+      "undefined",
+    ]);
+  });
+});
+
+// --->  FAIL  ./code-challenges.test.js
+//   noDups
+//   ✕ takes in two arrays as arguments and returns one array with no duplicate values.
+
+// ● noDups › takes in two arrays as arguments and returns one array with no duplicate values.
+
+//   ReferenceError: noDups is not defined
 
 // b) Create the function that makes the test pass.
--- ruby-challenges.rb
+++ ruby-challenges.rb
@@ -2,6 +2,8 @@
 # MINASWAN ✌️
 
 # --------------------1) Create a method that takes in a number and determines if the number is even or odd. Use the test variables provided.
+# psuedocode
+# I'm thinking I can use a simple if statment for this one. 
 
 reposts1 = 7
 # Expected output: '7 is odd'
@@ -10,10 +12,23 @@ reposts2 = 42
 reposts3 = 221
 # Expected output: '221 is odd'
 
-
+def even_or_odd(number)
+    if number % 2 == 0
+    p "#{number} is Even"
+    else
+    p "#{number} is Odd"
+    end
+end
+p even_or_odd(reposts1)
+p even_or_odd(reposts2)
+p even_or_odd(reposts3)
 
 # -------------------2) Create a method that takes in a string and removes all the vowels from the string. Use the test variables provided. 
 # HINT: Check out this resource: https://ruby-doc.org/core-2.6/String.html#method-i-delete
+# psuedocode
+# input: string
+# output: string with vowels removed
+# will remove vowels by using (/[aeiou]/, '') 
 
 beatles_album1 = 'Rubber Soul'
 # Expected output: 'Rbbr Sl'
@@ -22,9 +37,32 @@ beatles_album2 = 'Sgt Pepper'
 beatles_album3 = 'Abbey Road'
 # Expected output: 'bby Rd'
 
+def removed_vowels(string)
+    string.gsub(/[aeiou]/, '') 
+end
+
+p removed_vowels(beatles_album1)
+p removed_vowels(beatles_album2)
+p removed_vowels(beatles_album3)
+
+beatles_album1 = 'Rubber Soul'
+# Expected output: 'Rbbr Sl'
+beatles_album2 = 'Sgt Pepper'
+# Expected output: 'Sgt Pppr'
+beatles_album3 = 'Abbey Road'
+# Expected output: 'bby Rd'
+
+# ---> "Rbbr Sl"
+# "Sgt Pppr"
+# "Abby Rd"
 
 
 # -------------------3) Create a method that takes in a string and checks if the string is a palindrome. A palindrome is the same word spelled forward or backward. Use the test variables provided.
+# psuedocode:
+# Create a method that takes in a string and checks if the string is a palindrome. 
+# input: string
+# output: string
+# will reverse the strings with .reverse and use if statement to check to see if the oriiginal string == the reversed string  
 
 palindrome_tester1 = 'Racecar'
 # Expected output: 'Racecar is a palindrome'
@@ -32,3 +70,23 @@ palindrome_tester2 = 'LEARN'
 # Expected output: 'LEARN is not a palindrome'
 palindrome_tester3 = 'Rotator'
 # Expected output: 'Rotator is a palindrome'
+
+def is_or_not(string)
+    if string == string.reverse
+        p "#{string} is a palindrome"
+    else 
+        p "#{string} is not a palindrome"
+    end
+end
+
+p is_or_not(palindrome_tester1)
+p is_or_not(palindrome_tester2)
+p is_or_not(palindrome_tester3)
+
+# ---> "racecaR is not a palindrome"
+# "racecaR is not a palindrome"
+# "NRAEL is not a palindrome"
+# "NRAEL is not a palindrome"
+# "rotatoR is not a palindrome"
+# "rotatoR is not a palindrome"
+# I have no clue why I could not get it to return that racecar is a palindrome. But it appears that it is just printing 
